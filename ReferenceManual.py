e0dc1f3d2803af43042392f4391187a407ae583f
PREETI <preetikadyan634@gmail.com>
2021-02-23 12:36:16 +0530

    Update ReferenceManual.py

--- ReferenceManual.py
+++ ReferenceManual.py
@@ -1,6 +1,8 @@
 def num_corresponding_to_maj_min_color(MAJOR_COLORS,MINOR_COLORS):
+         num = 1
          for i in MAJOR_COLORS:
             for j in MINOR_COLORS:
-               print(i,j)
+               print(num,i,j)
+               num=num+1
              
                 
