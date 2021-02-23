9df658301405162a056297fe55a8ed495bc73d37
PREETI <preetikadyan634@gmail.com>
2021-02-23 12:00:19 +0530

    Add files via upload

---
+++ ReferenceManual.py
@@ -0,0 +1,8 @@
+def num_corresponding_to_maj_min_color(MAJOR_COLORS,MINOR_COLORS):
+
+     for num in range(1,26,1):
+
+         for i in MAJOR_COLORS:
+            for j in MINOR_COLORS:
+
+                print(num,i,j)
