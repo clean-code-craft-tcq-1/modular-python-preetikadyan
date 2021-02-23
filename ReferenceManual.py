6d60b8881f71b9fac4a78da81b2413b4973f7148
PREETI <preetikadyan634@gmail.com>
2021-02-23 12:56:23 +0530

    Update ReferenceManual.py

--- ReferenceManual.py
+++ ReferenceManual.py
@@ -3,10 +3,8 @@ def num_corresponding_to_maj_min_color(MAJOR_COLORS,MINOR_COLORS):
          num = 1
          for i in MAJOR_COLORS:
             for j in MINOR_COLORS:
-               print(num,i,j)
                mappingTable[num] = i + " " + j
                num=num+1
-         
-           displayMappingTable(mappingTable)
+               displayMappingTable(mappingTable)
              
                 
